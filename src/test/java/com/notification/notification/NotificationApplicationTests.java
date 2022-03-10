2f7c3131c0cea7bd8a7244e71205bc90f4be629c
juliannaPeace <juliannapazlo@gmail.com>
2022-03-09 21:34:04 -0300

    [TDD] add test doesn't send notification with minute wrong.

--- src/test/java/com/notification/notification/NotificationApplicationTests.java
+++ src/test/java/com/notification/notification/NotificationApplicationTests.java
@@ -54,6 +54,19 @@ class NotificationApplicationTests {
 	}
 
 	@ParameterizedTest
+	@ValueSource(ints = {1,10,5,15,2,3})
+	@DisplayName("When user is noob and hour is 12hs but minute doesn't 0, Then doesn't send specific notification")
+	void whenUserIsTypeNoobAndMinuteIsWrongThenDoesNotSendNotification(int minute) {
+
+		var typeUser = TypeUser.NOOB;
+		Mockito.when(localDateTimeFactory.now()).thenReturn(LocalDate.now().atTime(12,minute));
+
+		var response = notificationService.send(typeUser);
+
+		Assertions.assertThat(response.isPresent()).isFalse();
+	}
+
+	@ParameterizedTest
 	@ValueSource(ints = {19,9,8,11,15,20})
 	@DisplayName("When user is noob and hour doesn't 12hs or 18hs, Then doesn't send specific notification")
 	void whenUserIsTypeNoobAndTimeIsWrongThenDoesNotSendNotification(int hour) {
@@ -81,6 +94,19 @@ class NotificationApplicationTests {
 		Assertions.assertThat(response.get().numberDaysOfWeek()).isEqualTo(7);
 		Assertions.assertThat(response.get().message()).isEqualTo("Você está no caminho certo, continue acessando " +
 				"nosso APP para obter cada vez mais promoções");
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1,10,5,15,2,3})
+	@DisplayName("When user is intermediary and hour is 12hs but minute doesn't 0, Then doesn't send specific notification")
+	void whenUserIsTypeIntermediaryAndMinuteIsWrongThenDoesNotSendNotification(int minute) {
+
+		var typeUser = TypeUser.INTERMEDIARY;
+		Mockito.when(localDateTimeFactory.now()).thenReturn(LocalDate.now().atTime(12,minute));
+
+		var response = notificationService.send(typeUser);
+
+		Assertions.assertThat(response.isPresent()).isFalse();
 	}
 
 	@ParameterizedTest
